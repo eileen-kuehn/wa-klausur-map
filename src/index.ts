0351048a9d49fbb7e976852e9e47be68742776d3
Eileen Kuehn <eileen.kuehn@kit.edu>
2021-07-26 21:38:59 +0200

    fixed script

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ let currentPopup: any = undefined;
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
 
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
+WA.onEnterZone('clock', () => {
+    currentPopup =  WA.openPopup("clockPopup","It's " + time,[]);
 })
 
-WA.room.onLeaveZone('clock', closePopUp)
+WA.onLeaveZone('clock', closePopUp)
 
 function closePopUp(){
     if (currentPopup !== undefined) {
